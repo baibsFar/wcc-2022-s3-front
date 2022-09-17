54f7ecd4c79affdf688de3d12173c8ffceb84d4e
baibsFar <fararano.kevin@gmail.com>
2022-09-17 08:52:40 +0300

    Initial commit & Setting project requirements

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html, js}"],
+  theme: {
+    colors: {
+      'orange': ' #F49338',
+      'noir': '#292726',
+      'gris': '#F7F5F6',
+      'jaune': '#FFCE31'
+    },
+  },
+  plugins: [],
+}
